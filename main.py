b3dd7e28f5d3ef56fa3b18ef39ea20b564b6a436
Josh Rogge <joshrogge@gmail.com>
2026-04-15 16:21:55 -0500

    Yearly Income Prompt

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from decimal import Decimal, InvalidOperation
+def get_amount(prompt="Enter amount: "):
+    while True:
+        try:
+            return Decimal(input(prompt))
+        except InvalidOperation:
+            print("Invalid number, try again.")
+
 def prompt(text, pType):
-    pass
+    while True:
+        try:
+            return Decimal(input(text))
+        except InvalidOperation:
+            print("That's not a valid number format, please try again.")
 
 if __name__ == "__main__":
     print("Hello, lets create your budget!\n")
-    input("Yearly Income: \n")
+    yearly = prompt("Yearly Income: \n",type(40000))
+    monthly = yearly / 12
+    print(f"Great! That's {monthly:.2f} a month!")
